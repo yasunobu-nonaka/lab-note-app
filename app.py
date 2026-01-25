b691fad4c48b523a83e21e4ae5fce15aa1157409
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-01-25 19:51:56 +0900

    Add front page as /index and move the page that display note content to /note endpoint.
    app.py
    - add /note endpoint
    - change /index as front page
    
    HTML
    - create note.html to display markdown content in md note

--- app.py
+++ app.py
@@ -35,10 +35,15 @@ def md_to_html(md_string):
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/note")
+def note():
     with open("markdowns/sample.md", "r") as f:
         md_text = f.read()
     html_text = md_to_html(md_text)
-    return render_template("index.html", markdown=html_text)
+    return render_template("note.html", markdown=html_text)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
