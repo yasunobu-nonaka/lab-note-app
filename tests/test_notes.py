40ff44fe36daa7638037c7bd64eba71e3c2a2fa0
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-02-10 18:41:12 +0900

    Separate note creation process as a method

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -15,7 +15,6 @@ def register(client):
 
     return res
 
-
 def login(client):
     res = client.post(
         "/login",
@@ -28,6 +27,17 @@ def login(client):
 
     return res
 
+def create_note(client):
+    res = client.post(
+        "/notes",
+        data={
+            "title": "テストノート",
+            "content_md": "- 要素１\n- 要素２\n- 要素３",
+        },
+        follow_redirects=True,
+    )
+
+    return res
 
 def test_notes_index(client):
     register(client)
@@ -40,12 +50,5 @@ def test_note_creation(client):
     res = login(client)
     assert res.status_code == 200
 
-    res = client.post(
-        "/notes",
-        data={
-            "title": "テストノート",
-            "content_md": "- 要素１\n- 要素２\n- 要素３",
-        },
-        follow_redirects=True,
-    )
+    res = create_note(client)
     assert res.status_code == 200
