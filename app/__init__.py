de27e9670843c8dd5d70f93ed6ba3b93d5d1d10d
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-02-11 06:50:36 +0900

    Introduce python-dotenv
    run.py
    - add load_dotenv()
    - delete db.create_all() due to flask-migrate
    
    app/__init__.py
    - replace hard coded app config with os.environ.get() style

--- app/__init__.py
+++ app/__init__.py
@@ -13,10 +13,8 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-        SECRET_KEY="dev-secret-key", # TODO: os.environ.get()にする
-        SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-            app.instance_path, "notes.db"
-        ),
+        SECRET_KEY=os.environ.get("SECRET_KEY", "fallback-secret"),
+        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///notes.db"),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
