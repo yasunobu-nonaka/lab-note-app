c8b5fb118cc885fed9ff9e2c0727c8fc4d3cbd47
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-02-13 08:29:46 +0900

    small update: unify name and password used in test. rename test function name

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -14,53 +14,54 @@ def register_user(client, username, password, confirm):
     return res
 
 def test_register(client, app):
-    res = register_user(client, "lion", "lion12345678", "lion12345678")
+    res = register_user(client, "shakesan", "oishishake1234", "oishishake1234")
     assert res.status_code == 200
     
     with app.app_context():
-        user = User.query.filter_by(username="lion").first()
+        user = User.query.filter_by(username="shakesan").first()
         assert user is not None
 
 
-def test_short_password_rejected(client, app):
-    res = register_user(client, "lion", "lion1234", "lion1234")
+def test_short_password_register_rejected(client, app):
+    res = register_user(client, "shakesan", "shake1234", "shake1234")
     assert res.status_code == 200
     
     with app.app_context():
-        user = User.query.filter_by(username="lion").first()
+        user = User.query.filter_by(username="shakesan").first()
         assert user is None
 
-def test_no_confirm_rejected(client, app):
-    res = register_user(client, "lion", "lion1234", "")
+def test_no_confirm_register_rejected(client, app):
+    res = register_user(client, "shakesan", "shake1234", "")
     assert res.status_code == 200
     
     with app.app_context():
-        user = User.query.filter_by(username="lion").first()
+        user = User.query.filter_by(username="shakesan").first()
         assert user is None
 
 
-def test_duplicate_username_rejected(client, app):
+def test_duplicate_username_register_rejected(client, app):
     test_register(client, app)
 
-    res = register_user(client, "lion", "password1234", "password1234")
+    res = register_user(client, "shakesan", "password1234", "password1234")
     assert res.status_code == 200
 
     with app.app_context():
-        users = User.query.filter_by(username="lion").all()
+        users = User.query.filter_by(username="shakesan").all()
         assert len(users) == 1
 
 
 def test_login(client, app):
-    register_user(client, "newshake", "newshake1234", "newshake1234")
+    register_user(client, "shakesan", "oishishake1234", "oishishake1234")
 
     res = client.post(
         "/login",
         data={
-            "username": "newshake",
-            "password": "newshake1234",
+            "username": "shakesan",
+            "password": "oishishake1234",
         },
         follow_redirects=True,
     )
 
     assert len(res.history) == 1
     assert res.status_code == 200
+
